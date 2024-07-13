310261fcf06dc88d52cd157a327bd67b13cb2b28
Edilando Saturnino <ediralando@gmail.com>
2024-07-13 19:18:45 -0300

    ✨ feat: Add slash command for USD exchange rate and automatic scheduling

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const { Client, GatewayIntentBits } = require('discord.js')
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js')
 const axios = require('axios')
 const cron = require('node-cron')
 require('dotenv').config()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-client.once('ready', () => {
+client.once('ready', async () => {
   console.log(`Online!`)
+
+  const commands = [
+    {
+      name: 'cotacao',
+      description: 'Obtenha a cotação atual do dólar',
+    },
+  ]
+
+  const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
+  try {
+    console.log('Started refreshing application (/) commands.')
+
+    await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID), { body: commands })
+
+    console.log('Successfully reloaded application (/) commands.')
+  } catch (error) {
+    console.error(error)
+  }
 
   cron.schedule(
     '0 9 * * *',
@@ -69,6 +87,18 @@ client.once('ready', () => {
   )
 })
 
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return
+
+  const { commandName } = interaction
+
+  if (commandName === 'cotacao') {
+    await interaction.deferReply()
+    const rateMessage = await getDollarRate()
+    await interaction.editReply(rateMessage)
+  }
+})
+
 async function sendDollarRate() {
   try {
     const response = await axios.get('https://api.exchangerate-api.com/v4/latest/USD')
@@ -80,6 +110,17 @@ async function sendDollarRate() {
     }
   } catch (error) {
     console.error('Erro ao buscar a cotação do dólar:', error)
+  }
+}
+
+async function getDollarRate() {
+  try {
+    const response = await axios.get('https://api.exchangerate-api.com/v4/latest/USD')
+    const rate = response.data.rates.BRL
+    return `A cotação atual do dólar é R$ ${rate.toFixed(2)}`
+  } catch (error) {
+    console.error('Erro ao buscar a cotação do dólar:', error)
+    return 'Erro ao buscar a cotação do dólar'
   }
 }
 
